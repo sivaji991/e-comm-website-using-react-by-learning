c3c8a00179385ad9d54cdd4a59f032865af97596
Sivaji Avulamanda <sivajisalaar@gmail.com>
2026-01-20 07:29:51 +0530

    move all the checkout components into checkout folder

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import CheckoutPage from './pages/CheckoutPage'
+import CheckoutPage from './pages/checkout/CheckoutPage'
 import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router'
 import OrdersPage from './pages/OrdersPage'
